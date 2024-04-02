10eff3522a8760d4913e18cb30c2377773d99e74
Keanu Tibule <keanudane@yahoo.com>
2024-04-02 14:43:25 +0800

    initialized API

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,52 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [cityName, setCityName] = useState('');
+  const [weatherData, setWeatherData] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
+  
+  const handleInputChange = (e) => {
+    setCityName(e.target.value);
+  };
+
+  const getWeather = () => {
+    const apiUrl = `https://open-weather13.p.rapidapi.com/city/${cityName}`;
+
+    axios.get(apiUrl, {
+      headers: {
+        'x-rapidapi-key': 'ac2cf038b8mshd1ce619da8edd1ep1dd527jsna36260c1f56f',
+        'x-rapidapi-host': 'open-weather13.p.rapidapi.com'
+      }
+    })
+    .then(response => {
+      console.log(response.data);
+      setWeatherData(response.data);
+      setErrorMessage(null);
+    })
+    .catch(error => {
+      setErrorMessage(error.meessage);
+      setWeatherData(null)
+    });
+  };
+  
+  // useEffect(() => {
+  //   console.log(weatherData?.weather[0]?.main)
+  // }, [weatherData])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='container'>
+        <header>
+          <h1>How's the Weather Today?</h1>
+        </header>
+        <input type="text" value={cityName} onChange={handleInputChange} placeholder="Enter city name" />
+        <button onClick={getWeather}>Get Weather</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
